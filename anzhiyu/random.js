bd1d55e035981e222f67dc63550e1ad64209649c
hiter-zou <310205102@qq.com>
2024-09-21 11:34:25 +0800

    Site updated: 2024-09-21 11:34:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/25/My-New-Post/","2024/08/23/hello-world/","2024/08/25/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/08/25/My-New-Post/","2024/08/23/hello-world/","2024/08/25/这是一篇新的博文/","2024/09/21/外星人入侵/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
